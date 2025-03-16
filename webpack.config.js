eeac6156dbfb20cc95f853a5df7a700634f8c795
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-03-15 20:14:18 -0600

    add in webpack.js publickpath

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ export default {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/BattleShip-App',
         clean: true,
         assetModuleFilename: 'images/[name][ext]',
     },
